630e2b8d52342827cdc26809964422881a37ebc4
Christopher Teixeira <chris@christopherteixeira.com>
2023-12-09 17:32:28 -0500

    cleaned up code

--- day 3/day3.py
+++ day 3/day3.py
@@ -8,22 +8,14 @@ schematic = list(open(f,'r'))
 h = len(schematic)
 w = len(schematic[0])
 
-# print(h)
-# print(w)
-
 symbols = {(row,col): [] for row in range(h) for col in range(w-1) if schematic[row][col] not in '.1234567890'}
 gears = {(row,col): [] for row in range(h) for col in range(w-1) if schematic[row][col]=="*"}
-
-# print(schematic)
-# print(symbols)
 
 for i,row in enumerate(schematic):
     for j in re.finditer(r'\d+', row):
         edge = {(r, c) for r in (i-1, i, i+1)
                        for c in range(j.start()-1, j.end()+1)}
-        
-        # print(f"({i},{row}): {edge}")
-        
+                
         for k in edge & symbols.keys():
             symbols[k].append(int(j.group()))
 
@@ -31,7 +23,5 @@ for i,row in enumerate(schematic):
             gears[k].append(int(j.group()))    
 
 
-# print(symbols)
 print(f'Sum of part numbers: {sum(sum(part) for part in symbols.values())}')
-
 print(f'Product of part numbers: {sum(math.prod(part) for part in gears.values() if len(part)==2)}')
