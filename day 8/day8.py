7994e6e883a0b57de57846976daf04277f7d3dba
Christopher Teixeira <chris@christopherteixeira.com>
2023-12-13 20:41:22 -0500

    day 8 complete!

--- day 8/day8.py
+++ day 8/day8.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 from pprint import pprint
 
 f = 'day 8/day8.txt'
@@ -11,30 +11,30 @@ instructions = lines[0]
 network = dict()
 
 for l in lines[2:]:
-    # print(l)
     n = re.findall(r'(\w+)',l)
-    # print(n)
     network[n[0]] = {'L':n[1],'R':n[2]}
 
-pprint(instructions)
-pprint(network)
-
-next_node = 'AAA'
-steps = 0
-i = 0
-while True:
-    # if i == 0:
-    #     next_node = network['AAA'][d]
-    # else: 
-    #     next_node = network[next_node][d]
-    if i >= len(instructions): i = 0
-
-    d = instructions[i]
-    next_node = network[next_node][d]
-    # print(i)
-    steps +=1
-    i += 1
+def get_steps(network=network,instructions=instructions,next_node='AAA',part1=True):
     
-    if next_node == 'ZZZ': break
+    steps = 0
+    i = 0
+    while True:
+        if i >= len(instructions): i = 0
 
-print(f'Number of steps: {steps}')
+        d = instructions[i]
+        next_node = network[next_node][d]
+        steps +=1
+        i += 1
+        
+        if part1 and next_node == 'ZZZ': break
+        if not part1 and next_node[2] == 'Z':break 
+
+    return steps
+
+print(f'Part 1: Number of steps: {get_steps(next_node="AAA")}')
+
+starting_nodes = [x for x in network.keys() if x[2]=='A']
+
+part2_steps = [get_steps(next_node=n,part1=False) for n in starting_nodes]
+
+print(f'Part 2: Number of steps: {math.lcm(*part2_steps)}')
