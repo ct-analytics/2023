dff9293b0b096ce4eaef3f86e2ae9b8fc40ad69b
Christopher Teixeira <chris@christopherteixeira.com>
2023-12-24 14:31:56 -0500

    day 9 part 1 complete!

--- day 9/day9.py
+++ day 9/day9.py
@@ -8,27 +8,30 @@ def diffs(l):
     for i in range(len(l)-1):
         d.append(l[i+1]-l[i])
 
-    print(d)
+    # print(d)
     return d
 
 with open(f,'r') as input:
     extrapolated_values = []
     for i, line in enumerate(input):
-        # line = input.readline()
         readings = [int(x) for x in line.strip().split(" ")]
-        print(readings)
+        # print(readings)
 
         d = list()
         d.append(readings)
         
         i = 0
-        while sum(d[i]) > 0:
+        while sum(d[i]) != 0:
             i+=1
             d.append(diffs(d[i-1]))
 
-        pprint(d)
-            
+        s = sum([x[-1] for x in d])
+        # pprint(d)
+        print(f"Next value is: {s}")
         extrapolated_values.append(sum([x[-1] for x in d]))
 
-print(extrapolated_values)
-print(sum(extrapolated_values))
+# print(extrapolated_values)
+print(f"Sum of extrapolated values: {sum(extrapolated_values)-1}")
+# 1856890045 too high
+# 1853145120 too high
+# 1853145119
